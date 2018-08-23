83515d5db69b4e8bde54899b3e0d450da50ac039
Vinicius Cainelli <vicainelli@gmail.com>
2018-08-23 11:01:34 -0300

    Add new logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img class="h-10" alt="The VueJS News Logo" src="./assets/images/the-vuejs-news.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
