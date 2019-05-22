b121aa7e991db46b68dd36816a403e15595e95f4
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-05-22 20:55:33 -0300

    Implements Axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Axios from 'axios'
 
 Vue.config.productionTip = false
+
+Axios.defaults.baseURL = process.env.VUE_APP_NEWS_URL
 
 new Vue({
   router,
