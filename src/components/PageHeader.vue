f6707615b8551498de0f60e2b1024cce5598e756
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-05-22 10:23:25 -0300

    Adding some classes

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,17 +1,26 @@
 <template>
-  <div>
-    <div>
-      <img
-        class="h-16"
-        alt="The Vue News Logo"
-        src="../assets/images/logo.svg"
-      />
+  <div
+    class="flex justiy-between items-center px-3 border-b border-grey-100 mb-2"
+  >
+    <div class="items-center flex flex-1">
+      <router-link to="/"
+        ><img
+          class="h-16"
+          alt="The Vue News Logo"
+          src="../assets/images/logo.svg"
+      /></router-link>
+      <div class="flex-1 ml-3">
+        <div class="text-lg font-thin text-grey-600" v-if="home">Home</div>
+        <div v-if="category != ''">{{ category }}</div>
+      </div>
     </div>
     <div>
-      <div v-if="home">Home</div>
-      <div v-if="category != ''">{{ category }}</div>
+      <button
+        class="border border-grey-400 rounded-full text-xs px-3 py-1 text-grey-500 uppercase"
+      >
+        menu
+      </button>
     </div>
-    <div>menu</div>
   </div>
 </template>
 
