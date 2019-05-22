41a0a25c079ea83787e056b0e329a05805cf1cc0
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-05-21 22:55:16 -0300

    UPdates

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Vue News</h1>
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
